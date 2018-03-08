ce84b1cb0f2cc400f496ed91c28cd3bbadd6263a
tonyfloatersu <tonyfloater@gmail.com>
2018-03-08 13:21:45 +0800

    update package data specify

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.core import setup
 setup(
     name='LDADE',
     packages=['LDADE'],
+    package_dir={'LDADE': 'LDADE'},
+    package_data={'LDADE': ['data/*']},
     python_requires='>=2.7, < 3',
     install_requires=["numpy", "sklearn"]
 )
